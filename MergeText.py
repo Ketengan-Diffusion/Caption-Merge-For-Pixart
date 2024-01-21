a502f1fd6393f1a26ef023d8a1b093fe6d96eb45
tikhonlavrev <101032353+tikhonlavrev@users.noreply.github.com>
2024-01-21 22:36:41 +0700

    Fixed for non UTF-8 codec merging

--- MergeText.py
+++ MergeText.py
@@ -1,28 +1,38 @@
 import argparse
 import os
 
-# set up argument parser
+def read_file(file_path):
+    try:
+        # First attempt to read with utf-8
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return f.read().strip()
+    except UnicodeDecodeError:
+        # If utf-8 fails, attempt to read with 'windows-1252' encoding which covers ANSI
+        with open(file_path, 'r', encoding='windows-1252') as f:
+            return f.read().strip()
+
+def merge_files(txt_file, caption_file, output_file):
+    txt_content = read_file(txt_file)
+    caption_content = read_file(caption_file)
+    
+    if txt_content is not None and caption_content is not None:
+        with open(output_file, 'w', encoding='utf-8') as f_output:
+            f_output.write(txt_content + '\n' + caption_content + '\n')
+            print(f"Merged file {output_file} created successfully.")
+    else:
+        print("Failed to merge files due to an encoding error.")
+
+# Parse command line arguments
 parser = argparse.ArgumentParser(description='Merge .txt and .caption files into a single .txt file.')
 parser.add_argument('-s', '--source', required=True, help='Source directory with .txt and .caption files to merge.')
 parser.add_argument('-d', '--destination', required=True, help='Destination directory to save merged .txt files.')
 args = parser.parse_args()
 
-# function to merge files
-def merge_files(txt_file, caption_file, output_file):
-    with open(txt_file, 'r', encoding='utf-8') as f_txt, open(caption_file, 'r', encoding='utf-8') as f_caption:
-        txt_content = f_txt.read().strip()
-        caption_content = f_caption.read().strip()
-    
-    with open(output_file, 'w', encoding='utf-8') as f_output:
-        f_output.write(txt_content + '\n' + caption_content + '\n')
-
-# check if the source and destination directories exist
 if not os.path.exists(args.source):
     raise Exception(f"Source directory '{args.source}' does not exist.")
 if not os.path.exists(args.destination):
     os.makedirs(args.destination)
 
-# iterate over files in the source directory
 for file in os.listdir(args.source):
     if file.endswith('.txt'):
         base_name = os.path.splitext(file)[0]
@@ -30,9 +40,7 @@ for file in os.listdir(args.source):
         caption_file = os.path.join(args.source, base_name + '.caption')
         output_file = os.path.join(args.destination, file)
         
-        # check if the corresponding .caption file exists
         if not os.path.exists(caption_file):
             print(f"Warning: The corresponding caption file for '{txt_file}' is missing.")
         else:
             merge_files(txt_file, caption_file, output_file)
-            print(f"Merged file {output_file} created successfully.")
